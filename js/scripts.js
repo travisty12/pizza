1087f8f9b26048e1a6c001b780e198ca956bc100
Travis Toal <travisty12@gmail.com>
2019-02-01 09:53:15 -0800

    add basic buttons and form

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,25 @@ Pizza.prototype.cost = function() {
 
 $(document).ready(function() {
 
+  $("#infoBtn").click(function() {
+    $("#infoDiv").fadeOut();
+    setTimeout(function() {
+      $("#sizeDiv").fadeIn();
+    }, 400);
+  });
+
+  $("#sizeBtn").click(function() {
+    $("#sizeDiv").fadeOut();
+    setTimeout(function() {
+      $("#topDiv").fadeIn();
+    }, 400);
+  });
+
+  $("#topBtn").click(function() {
+    $("#topDiv").fadeOut();
+    setTimeout(function() {
+      $("#cartDiv").fadeIn();
+    }, 400);
+  });
+
 });
